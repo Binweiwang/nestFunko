a0f8f2eca31d3e7fae72279cd36bea51514d29f7
Binweiwang <binweiwang1029@gmail.com>
2023-12-18 19:52:44 +0100

    feat(main): Agregado ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import * as process from 'process'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix(process.env.API_VERSION || 'v1')
+  app.useGlobalPipes(new ValidationPipe())
   await app.listen(process.env.PORT || 3000)
 }
 bootstrap().then(() =>
