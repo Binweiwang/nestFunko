04c970335b9742f983b31f9e1d11fe6e5e2e7ea3
Binweiwang <binweiwang1029@gmail.com>
2023-12-18 19:51:37 +0100

    feat(funkomodule): Agregado funko-module

--- src/funko/funko.module.ts
+++ src/funko/funko.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { FunkoService } from './funko.service'
 import { FunkoController } from './funko.controller'
+import { NestFunkoMapper } from './mapper/funko.nestfunkomapper'
 
 @Module({
   controllers: [FunkoController],
-  providers: [FunkoService],
+  providers: [FunkoService, NestFunkoMapper],
 })
 export class FunkoModule {}
