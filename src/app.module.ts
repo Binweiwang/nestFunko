d898ef36ebf07ef3f761e18aa7da710170b5d10a
Binweiwang <binweiwang1029@gmail.com>
2023-12-18 19:49:27 +0100

    feat(app): Agregado mapper

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { ConfigModule } from '@nestjs/config'
 import { FunkoModule } from './funko/funko.module'
 import { FunkoController } from './funko/funko.controller'
 import { FunkoService } from './funko/funko.service'
+import { NestFunkoMapper } from './funko/mapper/funko.nestfunkomapper'
 
 @Module({
   imports: [ConfigModule.forRoot(), FunkoModule],
   controllers: [FunkoController],
-  providers: [FunkoService],
+  providers: [FunkoService, NestFunkoMapper],
 })
 export class AppModule {}
