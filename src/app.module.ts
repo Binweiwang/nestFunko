0b34eb9a07b239424d2b3f300383199a05bf0f63
Binweiwang <binweiwang1029@gmail.com>
2024-01-06 17:35:59 +0100

    feat(app.module.ts): Agregado app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,27 @@ import { ConfigModule } from '@nestjs/config'
 import { FunkoModule } from './funko/funko.module'
 import { FunkoController } from './funko/funko.controller'
 import { FunkoService } from './funko/funko.service'
-import { NestFunkoMapper } from './funko/mapper/funko.nestfunkomapper'
+import { FunkoMapper } from './funko/mapper/funko.nestfunkomapper'
+import { CategoriasModule } from './categorias/categorias.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [ConfigModule.forRoot(), FunkoModule],
-  controllers: [FunkoController],
-  providers: [FunkoService, NestFunkoMapper],
+  imports: [
+    ConfigModule.forRoot(),
+    FunkoModule,
+    CategoriasModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'admin',
+      database: 'funkos',
+      entities: [`${__dirname}` + `/**/*.entity{.ts,.js}`],
+      synchronize: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
