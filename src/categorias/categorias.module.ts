46ce978251ccd5e4e5331c9cd58513dc2d0cabcc
Binweiwang <binweiwang1029@gmail.com>
2024-01-12 23:08:57 +0100

    feat(categorias.modules): Agregado modelo de cache

--- src/categorias/categorias.module.ts
+++ src/categorias/categorias.module.ts
@@ -5,10 +5,15 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { Categoria } from './entities/categoria.entity'
 import { CategoriasMapper } from './mapper/categorias.mapper'
 import { NotificationsModule } from '../websockets/notifications.module'
+import { CacheModule } from '@nestjs/cache-manager'
 
 @Module({
   // Importamos el servicio de categorias de TypeORM
-  imports: [TypeOrmModule.forFeature([Categoria]), NotificationsModule],
+  imports: [
+    TypeOrmModule.forFeature([Categoria]),
+    NotificationsModule,
+    CacheModule.register(),
+  ],
   controllers: [CategoriasController],
   providers: [CategoriasService, CategoriasMapper],
   exports: [],
