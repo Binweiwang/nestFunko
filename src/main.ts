0124c4f0dc98c7f4ac1668a8ae7c3f387c6f31f3
Binweiwang <binweiwang1029@gmail.com>
2024-01-18 15:41:19 +0100

    feat(main.ts): Agregado ssl

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import * as process from 'process'
 import { ValidationPipe } from '@nestjs/common'
+import { getSSLOptions } from './config/ssl/ssl.config'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const httpsOptions = getSSLOptions()
+  const app = await NestFactory.create(AppModule, { httpsOptions })
   app.setGlobalPrefix(process.env.API_VERSION || 'v1')
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(process.env.PORT || 3000)
