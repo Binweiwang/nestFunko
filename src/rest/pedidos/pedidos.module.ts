f3002cff5b8c399a77031f4ee3d1b666830e53f5
Binweiwang <binweiwang1029@gmail.com>
2024-01-18 15:50:07 +0100

    feat(pedidos_module): Agregado pedidos.module.ts

--- src/rest/pedidos/pedidos.module.ts
+++ src/rest/pedidos/pedidos.module.ts
@@ -2,15 +2,17 @@ import { Module } from '@nestjs/common'
 import { PedidosService } from './pedidos.service'
 import { PedidosController } from './pedidos.controller'
 import { MongooseModule, SchemaFactory } from '@nestjs/mongoose'
+import { Pedido } from './schemas/pedido.schema'
+import * as mongoosePaginate from 'mongoose-paginate-v2'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { CacheModule } from '@nestjs/cache-manager'
+import { Usuario } from '../users/entities/user.entity'
 import { Funko } from '../funko/entities/funko.entity'
-import { Pedido } from './schemas/pedido.schema'
 import { PedidosMapper } from './mapper/pedidos.mapper'
 
-function mongoosePaginate() {}
-
 @Module({
+  // El primer paso es en el módulo del recurso a paginar, debemos importar el plugin de paginación
+  // Esto lo hacemos así porque ya vamos a añadir el plugin de paginación a todos los esquemas
   imports: [
     MongooseModule.forFeatureAsync([
       {
@@ -22,8 +24,9 @@ function mongoosePaginate() {}
         },
       },
     ]),
-    TypeOrmModule.forFeature([Funko]),
-    CacheModule.register(),
+    TypeOrmModule.forFeature([Funko]), // Importamos el repositorio de Funkos
+    CacheModule.register(), // Importamos el módulo de cache
+    TypeOrmModule.forFeature([Usuario]), // Importamos el repositorio de usuarios
   ],
   controllers: [PedidosController],
   providers: [PedidosService, PedidosMapper],
